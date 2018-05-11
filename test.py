b568d562bfb1e5a90b340a458a89e43c1c81c9e7
brunobieberstein <38557310+brunobieberstein@users.noreply.github.com>
2018-05-11 12:24:32 +0200

    Add files via upload

---
+++ test.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri May 11 12:04:37 2018
+
+@author: student
+"""
+
+if set('ADC') - set('ATGC') != set():
+    print('no')
---
+++ untitled0.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Apr 20 10:25:01 2018
+
+@author: student
+"""
+
+x = input('Wer ist schwul? ')
+y = input('Wer ist cool? ')
+print(x ,'ist cool und',y, 'ist schwul\nDu Snitch <3')
