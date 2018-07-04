246755b3580ccfb2c20b905bde342d567fc65dbf
brunobieberstein <bruno.bieberstein@yahoo.com>
2018-07-04 11:47:50 +0200

    Alles

--- qft_main/__main__.py
+++ qft_main/__main__.py
@@ -1,27 +1,39 @@
+import sys
+import os
+
 import standardize
 import evaluate
 import parse_input
 
-fname = input('QFT-Datei zum Analysieren: ')
+fname = sys.argv[1]
+if os.path.isdir(fname):
+    fname = [os.path.join(fname, f) for f in sorted(os.listdir(fname))]
+else:
+    fname = [fname]
 
-data_in = open(fname, 'r')
-data, layout = parse_input.parse_input(data_in)
-data_in.close()
-
-conc, status = standardize.transform_to_concs(data, layout)
-
-patientrecords = parse_input.get_patient_records(conc, layout)
-
-print()
-for patient, record in sorted(patientrecords.items()):
-    test_result = evaluate.evaluate_patient(record)
-    if test_result == 1:
-        outcome = 'positive'
-    elif test_result == 0:
-        outcome = 'negative'
-    else:
-        outcome = 'nicht beurteilbar'
-    print ('Ergebnis four Patient', patient ,'ist', outcome + '.')
+for f in fname:
+    print()
+    print('Ergebnisse für Datei', f)
+    data_in = open(f, 'r')
+    data, layout = parse_input.parse_input(data_in)
+    data_in.close()
+    
+    conc, status = standardize.transform_to_concs(data, layout)
+    
+    patientrecords = parse_input.get_patient_records(conc, layout)
+    
+    for patient, record in sorted(patientrecords.items()):
+        test_result = evaluate.evaluate_patient(record)
+        if '--exclude-negative' in sys.argv:
+            if test_result == 0:
+                continue
+        if test_result == 1:
+            outcome = 'positive'
+        elif test_result == 0:
+            outcome = 'negative'
+        else:
+            outcome = 'nicht beurteilbar'
+        print ('Ergebnis für Patient', patient ,'ist', outcome + '.')
     
 print()
     
